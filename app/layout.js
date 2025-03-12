62a89c4e453ecff08027f82da8dd00b2e17c7072
kunalbamane57 <22bcs147@iiitdmj.ac.in>
2025-03-12 19:11:39 +0530

    added the transaction functionality with adding and deleting transactions

--- app/layout.js
+++ app/layout.js
@@ -2,30 +2,34 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AI Finance",
+  title: "Welth",
   description: "One stop Finance Platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        {/* header */}
-        <Header />
-        <main className="min-h-screen">{children}</main>
-        {/* footer */}
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made by Kunal</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with Kunal</p>
+            </div>
+          </footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
